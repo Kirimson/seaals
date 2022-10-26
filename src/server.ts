e142f07fc0812810aabc76f57a2d236f5cd59cdf
Kieran Gates <kierangates@outlook.com>
2022-10-26 17:36:50 +0100

    fix:custom title and favicon for swagger docs

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,12 @@ import path from "path";
 
 const port = process.env.PORT || 3000;
 
+const swaggerOptions = {
+  customSiteTitle: "SEaa(L)S API",
+  customfavIcon: "/favicon.ico",
+};
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, swaggerOptions));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
