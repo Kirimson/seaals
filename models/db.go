ac683ea9715d372f69e4cf1a7824fcf9c509f177
Kieran Gates <kierangates@outlook.com>
2025-10-07 18:59:49 +0100

    fix: create database file if it does not exist

--- models/db.go
+++ models/db.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	_ "modernc.org/sqlite"
@@ -14,6 +15,16 @@ func InitialiseDB(dbname string) (*sql.DB, error) {
 		return nil, err
 	}
 	fmt.Printf("Connecting to %s\n", dbPath)
+	_, err = os.Stat(dbPath)
+	if err != nil {
+		// Create the database
+		fmt.Printf("Database %s does not exist. Creating it...\n", dbPath)
+		_, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		return nil, err
